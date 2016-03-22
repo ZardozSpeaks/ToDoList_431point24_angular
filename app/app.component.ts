857b033ff7eaccb23210897127e954a127c5e3e3
David Remington and Rileston Nedrick Starnes III, esq <student@epicodus.com>
2016-03-22 11:39:10 -0700

    add basic views and controllers

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,91 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
-// defining annotation aka "decorator"
+//leaf view "decorator"//
+
 @Component({
-  selector: 'my-app',
+  selector: 'task-display',
+  inputs: ['task'],
   template: `
-  <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
-  </div>
-  ` //uses backticks
+    <h3>{{ task.description }}</h3>
+  `
 })
 
-export class AppComponent {
+//leaf controller//
 
+export class TaskComponent {
+  public task: Task;
+}
+
+//branch view "decorator"//
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
+  template:`
+  <task-display *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
+  `
+})
+
+//branch controller//
+
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log("child", clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
+
+//trunk view "decorator"//
+
+@Component({
+  selector: 'my-app',
+  directives: [TaskListComponent],
+  template: `
+  <div class="container">
+    <h1>To-Do List</h1>
+    <task-list
+      [taskList]="tasks"
+      (onTaskSelect)="taskWasSelected($event)">
+    </task-list>
+  </div>
+  `
+})
+
+//trunk controller//
+
+export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings Movies.", 2),
+      new Task("Do the laundry", 3)
+    ]
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log("parent", clickedTask);
+  }
+}
+
+//root model//
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number){
+
+  }
 }
